e540c93ccbcf2c34b8b2e9d1ff755d3a03cbf559
Manuel Salcedo <vm.salcedo@hotmail.com>
2023-07-10 11:34:08 -0500

    carga inicial 10072023 11:31

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+      navbar.classList.add('navbar-scroll');
+    } else {
+      navbar.classList.remove('navbar-scroll');
+    }
+  });
+
+
+  window.addEventListener('scroll', function() {
+    var myNavbar = document.getElementById('myNavbar');
+    var scrollPosition = window.scrollY || window.pageYOffset;
+  
+    if (scrollPosition > 0) {
+      myNavbar.classList.add('navbar-scrolled');
+    } else {
+      myNavbar.classList.remove('navbar-scrolled');
+    }
+  });
